2831881953fb7d9bf0a80eae6b37290a0c41301d
contactlifuna <126406458+contactlifuna@users.noreply.github.com>
2023-02-27 20:19:59 +0700

    Update random.sh

--- scripts/random.sh
+++ scripts/random.sh
@@ -28,19 +28,15 @@ install_3proxy() {
 gen_3proxy() {
     cat <<EOF
 daemon
-maxconn 20000
+maxconn 4000
 nscache 65536
-nscache6 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
-stacksize 2621440
 flush
-auth strong
+auth none # sửa thành auth none để bỏ qua authentication
 
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-$(awk -F "/" '{print "auth strong\n" \
-"allow " $1 "\n" \
+$(awk -F "/" '{print "allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
