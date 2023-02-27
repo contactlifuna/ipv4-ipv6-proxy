e9424ca5b23ab2d4df70f2d5eb97a4c3562a3110
contactlifuna <126406458+contactlifuna@users.noreply.github.com>
2023-02-27 19:59:29 +0700

    Update random.sh

--- scripts/random.sh
+++ scripts/random.sh
@@ -39,7 +39,6 @@ flush
 auth strong
 
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-$(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
@@ -48,7 +47,7 @@ EOF
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "$(random)/$(random)/$IP4/$port/$(gen64 $IP6)"
+        echo "//$IP4/$port/$(gen64 $IP6)"
     done
 }
 
